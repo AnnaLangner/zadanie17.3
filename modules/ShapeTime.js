d2acd16834c3daccb5aa37d0516a8982830131df
Ania <annalangner06@gmail.com>
2019-05-13 13:23:11 +0200

    first attempt to add colors

--- modules/ShapeTime.js
+++ modules/ShapeTime.js
@@ -1,14 +1,14 @@
-var os = require('os');
+// var os = require('os');
 
-function formatTime() {
-	var uptime = os.uptime();
-	if (n < 60) {
-		return (uptime%3600%60) + 's';
-	} if (n >= 60) {
-		return Math.floor((uptime%3600)/60) + 'min'
-	} if (n >= 3600) {
-		return Math.floor(uptime/3600) + 'h'
-	}
-}
+// function formatTime() {
+// 	var uptime = os.uptime();
+// 	if (n < 60) {
+// 		return (uptime%3600%60) + 's';
+// 	} if (n >= 60) {
+// 		return Math.floor((uptime%3600)/60) + 'min'
+// 	} if (n >= 3600) {
+// 		return Math.floor(uptime/3600) + 'h'
+// 	}
+// }
 
-exports.print = formatTime;
+// exports.print = formatTime;
