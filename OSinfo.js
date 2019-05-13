70de02f621b349ef0c9cb55322b291640f8f7491
Ania <annalangner06@gmail.com>
2019-05-13 12:25:26 +0200

    second attempt to add a time function

--- OSinfo.js
+++ OSinfo.js
@@ -15,9 +15,10 @@ function getOSinfo() {
     console.log('System:', type);
     console.log('Release:', release);
     console.log('CPU model:', cpu);
-    console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
+    console.log('Uptime: ~', ShapeTime.print() /*(uptime / 60).toFixed(0), 'min'*/);
     console.log('User name:', userInfo.username);
     console.log('Home dir:', userInfo.homedir);
 }
 
 exports.print = getOSinfo;
+
