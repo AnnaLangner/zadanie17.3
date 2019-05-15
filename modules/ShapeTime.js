dc5a66a8f46b1e477fc90a3791da3bf9bbfec08f
Ania <annalangner06@gmail.com>
2019-05-15 12:24:04 +0200

    the last improvement of the time function

--- modules/ShapeTime.js
+++ modules/ShapeTime.js
@@ -1,14 +1,10 @@
-var os = require('os');	
-var colors = require('colors');
+function ShapeTime(uptime) {
 
-function ShapeTime() {
-
-	var uptime = os.uptime();
 	var hours = Math.floor(uptime/3600).toFixed(0);
 	var minutes = Math.floor((uptime - hours*3600) /60).toFixed(0);
 	var seconds = Math.floor(uptime - hours*3600 -minutes*60).toFixed(0);
 	
-	console.log('Uptime: ~'.green, hours, 'h ', minutes, 'min ', seconds, 's');
+	return hours + 'h ' + minutes + 'min ' + seconds + 's';
 };
 
 exports.print = ShapeTime;
